fd174885e04abec36f2ba13ca4f1800f4b31d576
Bohui <bohui@tbhs-mbp.home>
2023-09-01 14:17:10 +0100

    update

--- Paper_Chat/apps.py
+++ Paper_Chat/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TextProcessorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'text_processor'
+    name = 'Paper_Chat'
